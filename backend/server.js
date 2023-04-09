f4d67d848373088988f697a9be18524e0d950e46
Virlus Jeanty <vjeanty02@gmail.com>
2023-04-09 14:39:51 -0500

    Database connection

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,14 @@ const port = process.env.PORT || 3000;
 // creation d'une application express
 const app = express(); 
 
+
+// Se connecter à la base de données mongodb, en utilisant la méthode mongoose.connect
+mongoose.connect('mongodb+srv://vjeanty02:jesus123@cluster0.1sagvzb.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+  
 // Middleware pour gérer les requêtes
 app.use((req, res, next) => {
     console.log('Réponse envoyée avec succès !');
