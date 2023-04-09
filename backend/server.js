9c85b752deaade1e90a65cc7c4f5746125a06d98
Virlus Jeanty <vjeanty02@gmail.com>
2023-04-09 13:54:43 -0500

    Create an Express Web Server

---
+++ backend/server.js
@@ -0,0 +1,6 @@
+//Etapes necessaires pour la creation d'un server web avec express
+
+// Installer les modules nécessaires
+// Middleware pour gérer les requêtes 
+// créer les routes 
+// démarre le serveur et affiche un message dans la console
