a1534cf81269654a1e8d56afde1681ebd2fbaec2
Virlus Jeanty <vjeanty02@gmail.com>
2023-04-09 14:48:29 -0500

    Create shema

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,12 @@
 // Installer les modules nécessaires
 const express = require('express'); 
 const mongoose = require('mongoose');
+
+// Définir le port d'écoute
 const port = process.env.PORT || 3000;
 
 // creation d'une application express
 const app = express(); 
-
 
 // Se connecter à la base de données mongodb, en utilisant la méthode mongoose.connect
 mongoose.connect('mongodb+srv://vjeanty02:jesus123@cluster0.1sagvzb.mongodb.net/?retryWrites=true&w=majority',
@@ -15,14 +16,25 @@ mongoose.connect('mongodb+srv://vjeanty02:jesus123@cluster0.1sagvzb.mongodb.net/
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-  
+
+// Définir un schéma pour les données
+const thingSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  inStock: { type: Boolean, required: true }
+});
+
+// Créer un modèle à partir du schéma
+const User = mongoose.model('Thing', thingSchema);
+
 // Middleware pour gérer les requêtes
 app.use((req, res, next) => {
     console.log('Réponse envoyée avec succès !');
     next();
   });
 
-// créer les routes 
+// créer les routes
 // démarre le serveur et affiche un message dans la console
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
