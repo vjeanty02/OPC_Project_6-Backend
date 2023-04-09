8b03e338cc679d1700cb997f9097fc974af96877
Virlus Jeanty <vjeanty02@gmail.com>
2023-04-09 15:13:50 -0500

    Add messages for requests

--- backend/server.js
+++ backend/server.js
@@ -35,17 +35,29 @@ app.use((req, res, next) => {
   });
 
 // Créer une route GET qui retournera tous les produits
-app.get('/api/sauces', (req, res) => {
+app.get('/sauces', (req, res) => {
   Thing.find()
-    .then(Products => res.status(200).json({products:Products}))
-    .catch(error => res.status(400).json({ error }));
+    .then(Sauces => {
+      console.log('GET request successful');
+      res.status(200).json({products:Sauces});
+    })
+    .catch(error => {
+      console.log('GET request failed. Error:', error);
+      res.status(400).json({ error });
+    });
 });
 
-// Créer une route GET qui retournera le produit avec le_id fourni
-app.get('/api/sauces/:id', (req, res) => {
+// Créer une route GET qui retournera le produit avec l'_id fourni
+app.get('/sauces/:id', (req, res) => {
   Thing.findOne({ _id: req.params.id })
-  .then(thing => res.status(200).json({product:thing}))
-  .catch(error => res.status(404).json({ error }));
+  .then(Sauce => {
+    console.log('GET:id request successful');
+    res.status(200).json({product:Sauce});
+  })
+  .catch(error => {
+    console.log('GET request failed. Error:', error);
+    res.status(404).json({ error });
+  });
 });
 
 // Démarre le serveur et affiche un message dans la console
