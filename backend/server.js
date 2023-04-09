db3329274d587feacdc7aab95fdb223690ce891d
Virlus Jeanty <vjeanty02@gmail.com>
2023-04-09 14:37:54 -0500

    Add a Middleware

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,20 @@
 // Installer les modules nécessaires
 const express = require('express'); 
 const mongoose = require('mongoose');
+const port = process.env.PORT || 3000;
 
 // creation d'une application express
 const app = express(); 
 
+// Middleware pour gérer les requêtes
+app.use((req, res, next) => {
+    console.log('Réponse envoyée avec succès !');
+    next();
+  });
 
-
-// Middleware pour gérer les requêtes 
 // créer les routes 
 // démarre le serveur et affiche un message dans la console
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
+
