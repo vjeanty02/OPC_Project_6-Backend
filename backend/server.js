78a28831082b24d3928d7dc3dd8bcb001dcc2129
Virlus Jeanty <vjeanty02@gmail.com>
2023-04-09 14:56:04 -0500

    Create route GET

--- backend/server.js
+++ backend/server.js
@@ -34,14 +34,21 @@ app.use((req, res, next) => {
     next();
   });
 
-// créer une route GET qui retournera tous les produits
-app.get('/api/products', (req, res) => {
+// Créer une route GET qui retournera tous les produits
+app.get('/api/sauces', (req, res) => {
   Thing.find()
     .then(Products => res.status(200).json({products:Products}))
     .catch(error => res.status(400).json({ error }));
 });
 
-// démarre le serveur et affiche un message dans la console
+// Créer une route GET qui retournera le produit avec le_id fourni
+app.get('/api/sauces/:id', (req, res) => {
+  Thing.findOne({ _id: req.params.id })
+  .then(thing => res.status(200).json({product:thing}))
+  .catch(error => res.status(404).json({ error }));
+});
+
+// Démarre le serveur et affiche un message dans la console
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
