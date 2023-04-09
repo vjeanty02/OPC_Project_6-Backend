70999678e71451b1b22265d01a80ba5e3a89e74d
Virlus Jeanty <vjeanty02@gmail.com>
2023-04-09 14:23:11 -0500

    Install the necessary modules

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
 //Etapes necessaires pour la creation d'un server web avec express
 
 // Installer les modules nécessaires
+const express = require('express'); 
+const mongoose = require('mongoose');
+
+// creation d'une application express
+const app = express(); 
+
+
+
 // Middleware pour gérer les requêtes 
 // créer les routes 
 // démarre le serveur et affiche un message dans la console
