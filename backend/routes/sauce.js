8e4b65b4915ae7b9874f9f7bfd54c422442d8034
Virlus Jeanty <vjeanty02@gmail.com>
2023-04-11 13:54:55 -0500

    Implement authentication middleware

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const sauceCtrl = require('../controllers/sauce.js')
+const auth = require('../middleware/auth');
 
-router.get('/', sauceCtrl.getAll);
-router.get('/:id', sauceCtrl.getOne);
-router.post('/', sauceCtrl.create);
-router.post('/:id/like', sauceCtrl.like);
-router.put('/:id', sauceCtrl.modify);
-router.delete('/:id', sauceCtrl.delete);
+router.get('/', auth, sauceCtrl.getAll);
+router.get('/:id', auth, sauceCtrl.getOne);
+router.post('/', auth, sauceCtrl.create);
+router.post('/:id/like', auth, sauceCtrl.like);
+router.put('/:id', auth, sauceCtrl.modify);
+router.delete('/:id', auth, sauceCtrl.delete);
 
 module.exports = router;
