47c400edfc050a58a17e58dc0523635549f27d8b
Virlus Jeanty <vjeanty02@gmail.com>
2023-04-11 10:31:52 -0500

    Update sauce.js

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -2,20 +2,10 @@ const express = require('express');
 const router = express.Router();
 const sauceCtrl = require('../controllers/sauce.js')
 
-// Créer une route GET qui retournera tous les produits
 router.get('/', sauceCtrl.getAll);
-
-// Créer une route GET qui retournera le produit avec l'_id fourni
 router.get('/:id', sauceCtrl.getOne);
-
-// Créer une route POST qui créera un nouveau Product dans la base de données, en utilisant la méthode app.post d’express
 router.post('/', sauceCtrl.create);
-
-// Créer une route PUT qui modifiera le produit avec l'_id fourni
 router.put('/:id', sauceCtrl.modify);
-
-// Créer une route DELETE qui supprimera le produit avec le _id fourni
 router.delete('/:id', sauceCtrl.delete);
-
 
 module.exports = router;
