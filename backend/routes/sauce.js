7c8ec30711e17014893036cb2a7ec31b8018b24c
Virlus Jeanty <vjeanty02@gmail.com>
2023-04-11 11:31:44 -0500

    Update sauce.js

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -5,6 +5,7 @@ const sauceCtrl = require('../controllers/sauce.js')
 router.get('/', sauceCtrl.getAll);
 router.get('/:id', sauceCtrl.getOne);
 router.post('/', sauceCtrl.create);
+router.post('/:id/like', sauceCtrl.like);
 router.put('/:id', sauceCtrl.modify);
 router.delete('/:id', sauceCtrl.delete);
 
