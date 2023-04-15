972321be5812545afe9055b682baec4fd1ca5f86
Virlus Jeanty <vjeanty02@gmail.com>
2023-04-14 23:37:11 -0500

    Use limiter

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const userCtrl = require('../controllers/user.js');
+const limiter = require('../middleware/limiter');
 
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/signup', limiter, userCtrl.signup);
+router.post('/login', limiter, userCtrl.login);
 
 module.exports = router;
